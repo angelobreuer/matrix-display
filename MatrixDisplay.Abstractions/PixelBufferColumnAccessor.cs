414c8c10b8b2a9d6e664a6db931e0d157f46c8d0
Angelo Breuer <angelobr@outlook.de>
2022-11-09 20:55:18 +0100

    feat: Add index and range helpers for column and row accessors

--- MatrixDisplay.Abstractions/PixelBufferColumnAccessor.cs
+++ MatrixDisplay.Abstractions/PixelBufferColumnAccessor.cs
@@ -2,26 +2,47 @@
 
 public readonly record struct PixelBufferColumnAccessor(PixelBuffer Buffer)
 {
-    public Color this[int offset]
+    public Color this[Index index]
     {
         set
         {
-            if (offset >= Buffer.Width)
-            {
-                return ;
-            }
-
-            var position = offset;
-            var span = Buffer.Data;
-
-            do
-            {
-                span[position] = value;
-                position += Buffer.Width;
-            }
-            while (position < span.Length);
+            var offset = index.GetOffset(Buffer.Width);
+            SetColumn(offset, value);
 
             Buffer.MarkDirty();
         }
+    }
+
+    public Color this[Range range]
+    {
+        set
+        {
+            var (offset, length) = range.GetOffsetAndLength(Buffer.Width);
+
+            for (var xOffset = 0; offset < length; xOffset++)
+            {
+                SetColumn(offset + xOffset, value);
+            }
+
+            Buffer.MarkDirty();
+        }
+    }
+
+    private void SetColumn(int offset, Color value)
+    {
+        if (offset >= Buffer.Width)
+        {
+            return;
+        }
+
+        var position = offset;
+        var span = Buffer.Data;
+
+        do
+        {
+            span[position] = value;
+            position += Buffer.Width;
+        }
+        while (position < span.Length);
     }
 }
--- MatrixDisplay.Abstractions/PixelBufferRowAccessor.cs
+++ MatrixDisplay.Abstractions/PixelBufferRowAccessor.cs
@@ -2,20 +2,42 @@
 
 public readonly record struct PixelBufferRowAccessor(PixelBuffer Buffer)
 {
-    public Color this[int offset]
+    public Color this[Index index]
     {
         set
         {
-            if (offset >= Buffer.Height)
-            {
-                return;
-            }
+            var offset = index.GetOffset(Buffer.Height);
+            SetRow(offset, value);
 
-            var startPosition = offset * Buffer.Width;
-            var endPosition = (offset + 1) * Buffer.Height;
-
-            Buffer.Data[startPosition..endPosition].Fill(value);
             Buffer.MarkDirty();
         }
+    }
+
+    public Color this[Range range]
+    {
+        set
+        {
+            var (offset, length) = range.GetOffsetAndLength(Buffer.Height);
+
+            for (var yOffset = 0; offset < length; yOffset++)
+            {
+                SetRow(offset + yOffset, value);
+            }
+
+            Buffer.MarkDirty();
+        }
+    }
+
+    private void SetRow(int offset, Color value)
+    {
+        if (offset >= Buffer.Height)
+        {
+            return;
+        }
+
+        var startPosition = offset * Buffer.Width;
+        var endPosition = (offset + 1) * Buffer.Height;
+
+        Buffer.Data[startPosition..endPosition].Fill(value);
     }
 }
