4444c4b38e4518a6d6b6845832ccd87989463764
Angelo Breuer <angelobr@outlook.de>
2022-11-11 13:24:13 +0100

    feat(apps): Allow to specify scrolling text

--- apps/MatrixDisplay.Apps.ScrollingText/Program.cs
+++ apps/MatrixDisplay.Apps.ScrollingText/Program.cs
@@ -1,18 +1,20 @@
 ﻿using System.Drawing;
 using MatrixDisplay;
 
-const string Text = "Hallo Welt";
+var text = args.Length is 0
+    ? "Hallo Welt"
+    : string.Join(' ', args);
+
 const double ScrollSpeed = 2D;
 
 var buffer = PixelBuffer.Instance;
 using var font = new Font("Arial", buffer.Height);
-var size = buffer.MeasureText(font, Text);
-
+var size = buffer.MeasureText(font, text);
 var offset = 0D;
 
 while (true)
 {
-    buffer.DrawText(font, Text, (int)Math.Round(offset));
+    buffer.DrawText(font, text, (int)Math.Round(offset));
     offset -= ScrollSpeed;
 
     if (offset < -size.Width)
