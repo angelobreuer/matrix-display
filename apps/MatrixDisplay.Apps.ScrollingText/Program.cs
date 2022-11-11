cecc53d67e0d2efd8a679af21198652e8fde1166
Angelo Breuer <angelobr@outlook.de>
2022-11-11 08:54:05 +0100

    feat(apps): Add scrolling text app

---
+++ apps/MatrixDisplay.Apps.ScrollingText/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using MatrixDisplay;
+
+const string Text = "Hallo Welt";
+const double ScrollSpeed = 2D;
+
+var buffer = PixelBuffer.Instance;
+using var font = new Font("Arial", buffer.Height);
+var size = buffer.MeasureText(font, Text);
+
+var offset = 0D;
+
+while (true)
+{
+    buffer.DrawText(font, Text, (int)Math.Round(offset));
+    offset -= ScrollSpeed;
+
+    if (offset < -size.Width)
+    {
+        offset = size.Width;
+    }
+
+    Thread.Sleep(100);
+}
