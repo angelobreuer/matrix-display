74e16bd827fb37a55245218bece8f10036abd822
Angelo Breuer <angelobr@outlook.de>
2022-11-16 19:24:51 +0100

    feat(apps): Add shifting pattern effect

---
+++ apps/MatrixDisplay.Apps.ChristmasTree/SimpleShiftingPatternEffect.cs
@@ -0,0 +1,13 @@
+﻿namespace MatrixDisplay.Apps.ChristmasTree;
+
+using System.Collections.Generic;
+using MatrixDisplay;
+
+internal sealed class SimpleShiftingPatternEffect : ShiftingPatternEffectBase
+{
+    protected override IEnumerable<PatternSegment> GetPattern()
+    {
+        yield return new PatternSegment(Color.Red, 3);
+        yield return new PatternSegment(Color.Green, 2);
+    }
+}
