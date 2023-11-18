07ee716a5b0402eec47a6f39593391f72e8c2ed2
Angelo Breuer <angelobr@outlook.de>
2023-11-18 07:27:31 +0100

    feat: Add text sample

---
+++ apps/Matrix.Apps.Text/Program.cs
@@ -0,0 +1,14 @@
+﻿using MatrixSdk;
+using SkiaSharp;
+
+var buffer = ImageBuffer.Create();
+using var bitmap = new SKBitmap(buffer.Width, buffer.Height);
+
+using var canvas = new SKCanvas(bitmap);
+using var typeface = SKTypeface.FromFamilyName("Arial");
+using var font = new SKFont(typeface);
+using var paint = new SKPaint(font) { Color = SKColors.Red, };
+canvas.DrawText("H", default, paint);
+
+buffer.DrawImage(bitmap);
+buffer.Commit();
