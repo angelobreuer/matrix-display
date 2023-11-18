5ac4b0fa55944d98f7d218d96c887356b241e9e7
Angelo Breuer <angelobr@outlook.de>
2023-11-18 07:27:04 +0100

    feat(apps): Add test app

---
+++ apps/Matrix.Apps.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using MatrixSdk;
+
+var buffer = ImageBuffer.Create();
+
+void TestColor(Color color)
+{
+    buffer.Colors.Fill(color);
+    buffer.Commit();
+
+    Console.WriteLine($"Testing color {color}");
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+}
+
+TestColor(new Color(255, 0, 0));
+TestColor(new Color(0, 255, 0));
+TestColor(new Color(0, 0, 255));
+TestColor(new Color(255, 255, 255));
